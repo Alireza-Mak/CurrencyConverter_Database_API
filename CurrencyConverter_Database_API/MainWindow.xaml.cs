6b7d816e959b4158c90f108d137dceab9168c446
Alireza Mak <info@alirezamak.com>
2025-08-31 16:54:19 -0700

    Added BindCurrencyData() to populate currency ComboBoxes

--- CurrencyConverter_Database_API/MainWindow.xaml.cs
+++ CurrencyConverter_Database_API/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace CurrencyConverter_Database_API
 {
@@ -10,10 +13,104 @@ namespace CurrencyConverter_Database_API
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection sqlConnection;
+
+        private SqlCommand sqlCommand;
+
+        private SqlDataAdapter sqlDataAdapter;
+
         public MainWindow()
         {
             InitializeComponent();
             SetCurrentYear();
+
+            // Initialize the database connection and bind currency data to the DataGrid
+            BindCurrencyData();
+
+        }
+
+        /// <summary>
+        /// This method establishes a connection to the database using the connection string from the configuration file.
+        /// We can then use this connection to execute SQL commands and queries.
+        /// </summary>
+        public void DbConnection()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method retrieves currency data from the Currency_Master table in the database and 
+        /// binds it to two ComboBoxes (cmbFromCurrency and cmbToCurrency).
+        /// </summary>
+        public void BindCurrencyData()
+        {
+            DbConnection();
+            try
+            {
+                // Create a DataTable to hold the data from the Currency_Master table
+                DataTable dataTable = new DataTable();
+
+                string query = "SELECT * FROM Currency_Master";
+
+                // Create a query to get data from Currency_Master table
+                sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Create a SqlDataAdapter to execute the query and fill the DataTable
+                sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(dataTable);
+
+
+                DataRow newRow = dataTable.NewRow();
+                newRow["Id"] = 0;
+
+                newRow["CurrencyName"] = "--SELECT--";
+
+                dataTable.Rows.InsertAt(newRow, 0);
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    // Assign the DataTable to cmbFromCurrency and cmbToCurrency ComboBoxes
+                    cmbFromCurrency.ItemsSource = dataTable.DefaultView;
+                    cmbToCurrency.ItemsSource = dataTable.DefaultView;
+                }
+                sqlConnection.Close();
+
+                // Set DisplayMemberPath and SelectedValuePath for both ComboBoxes and set default selected index to 0
+                cmbFromCurrency.DisplayMemberPath = "CurrencyName";
+                cmbFromCurrency.SelectedValuePath = "Id";
+                cmbFromCurrency.SelectedIndex = 0;
+
+                // Set DisplayMemberPath and SelectedValuePath for both ComboBoxes and set default selected index to 0
+                cmbToCurrency.DisplayMemberPath = "CurrencyName";
+                cmbToCurrency.SelectedValuePath = "Id";
+                cmbToCurrency.SelectedIndex = 0;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
 
         public void Save_Click(object sender, RoutedEventArgs e)
